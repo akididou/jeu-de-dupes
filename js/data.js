e40ab789d971e427c1fb1c5e96d145d70a050578
akididou <jp.poizat@hotmail.fr>
2024-11-15 01:54:22 +0100

    WIP

---
+++ js/data.js
@@ -0,0 +1,416 @@
+const parfumsData = {
+    femme: [
+        {
+            "numero": "101",
+            "nom": "SI",
+            "marque": "ARMANI"
+        },
+        {
+            "numero": "515",
+            "nom": "ROSE D'ARABIE - PRIVÉ",
+            "marque": "ARMANI"
+        },
+        {
+            "numero": "163",
+            "nom": "CH",
+            "marque": "CAROLINA HERRERA"
+        },
+        {
+            "numero": "113",
+            "nom": "N°5",
+            "marque": "CHANEL"
+        },
+        {
+            "numero": "114",
+            "nom": "COCO MADEMOISELLE",
+            "marque": "CHANEL"
+        },
+        {
+            "numero": "115",
+            "nom": "CHANCE",
+            "marque": "CHANEL"
+        },
+        {
+            "numero": "116",
+            "nom": "COCO",
+            "marque": "CHANEL"
+        },
+        {
+            "numero": "176",
+            "nom": "N°5 EAU PREMIERE",
+            "marque": "CHANEL"
+        },
+        {
+            "numero": "181",
+            "nom": "COCO NOIR",
+            "marque": "CHANEL"
+        },
+        {
+            "numero": "121",
+            "nom": "ADDICT",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "122",
+            "nom": "J'ADORE",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "123",
+            "nom": "MISS DIOR CHÉRIE",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "124",
+            "nom": "HYPNOTIC POISON",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "125",
+            "nom": "POISON GIRL",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "180",
+            "nom": "JOY",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "185",
+            "nom": "MISS DIOR",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "510",
+            "nom": "BOIS D'ARGENT",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "512",
+            "nom": "OUD ISPAHAN",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "129",
+            "nom": "MAGNETISM",
+            "marque": "ESCADA"
+        },
+        {
+            "numero": "505",
+            "nom": "BACCARAT ROUGE 540",
+            "marque": "FRANCIS KURKDJIAN"
+        },
+        {
+            "numero": "133",
+            "nom": "L'INTERDIT",
+            "marque": "GIVENCHY"
+        },
+        {
+            "numero": "135",
+            "nom": "LA PETITE ROBE NOIRE",
+            "marque": "GUERLAIN"
+        },
+        {
+            "numero": "137",
+            "nom": "SHALIMAR",
+            "marque": "GUERLAIN"
+        },
+        {
+            "numero": "173",
+            "nom": "L'INSTANT",
+            "marque": "GUERLAIN"
+        },
+        {
+            "numero": "182",
+            "nom": "LA PETITE ROBE NOIRE NECTAR",
+            "marque": "GUERLAIN"
+        },
+        {
+            "numero": "175",
+            "nom": "BOSS MA VIE",
+            "marque": "HUGO BOSS"
+        },
+        {
+            "numero": "140",
+            "nom": "LA BELLE",
+            "marque": "JEAN PAUL GAULTIER"
+        },
+        {
+            "numero": "141",
+            "nom": "CLASSIQUE",
+            "marque": "JEAN PAUL GAULTIER"
+        },
+        {
+            "numero": "142",
+            "nom": "SCANDAL",
+            "marque": "JEAN PAUL GAULTIER"
+        },
+        {
+            "numero": "171",
+            "nom": "MYRRH & TONKA",
+            "marque": "JO MALONE"
+        },
+        {
+            "numero": "144",
+            "nom": "FLOWER",
+            "marque": "KENZO"
+        },
+        {
+            "numero": "147",
+            "nom": "TRESOR",
+            "marque": "LANCOME"
+        },
+        {
+            "numero": "150",
+            "nom": "LA VIE EST BELLE",
+            "marque": "LANCOME"
+        },
+        {
+            "numero": "178",
+            "nom": "AIRE",
+            "marque": "LOEWE"
+        },
+        {
+            "numero": "179",
+            "nom": "NARCISO POUDRÉ",
+            "marque": "NARCISO RODRIGUEZ"
+        },
+        {
+            "numero": "160",
+            "nom": "LADY MILLION",
+            "marque": "PACO RABANNE"
+        },
+        {
+            "numero": "161",
+            "nom": "BLACK XS",
+            "marque": "PACO RABANNE"
+        },
+        {
+            "numero": "162",
+            "nom": "OLYMPÉA",
+            "marque": "PACO RABANNE"
+        },
+        {
+            "numero": "189",
+            "nom": "PURE XS FOR HER",
+            "marque": "PACO RABANNE"
+        },
+        {
+            "numero": "104",
+            "nom": "ANGEL NOVA",
+            "marque": "THIERRY MUGLER"
+        },
+        {
+            "numero": "154",
+            "nom": "ALIEN",
+            "marque": "THIERRY MUGLER"
+        },
+        {
+            "numero": "155",
+            "nom": "ANGEL",
+            "marque": "THIERRY MUGLER"
+        },
+        {
+            "numero": "183",
+            "nom": "ALIEN GODDESS",
+            "marque": "THIEERY MUGLER"
+        },
+        {
+            "numero": "165",
+            "nom": "FLOWER BOMB",
+            "marque": "VIKTOR & ROLF"
+        },
+        {
+            "numero": "167",
+            "nom": "MANIFESTO",
+            "marque": "YVES SAINT LAURENT"
+        },
+        {
+            "numero": "168",
+            "nom": "BLACK OPIUM",
+            "marque": "YVES SAINT LAURENT"
+        },
+        {
+            "numero": "169",
+            "nom": "MON PARIS",
+            "marque": "YVES SAINT LAURENT"
+        },
+        {
+            "numero": "172",
+            "nom": "LIBRE",
+            "marque": "YVES SAINT LAURENT"
+        }
+    ],
+    homme: [
+        {
+            "numero": "301",
+            "nom": "ACQUA DI GIO",
+            "marque": "ARMANI"
+        },
+        {
+            "numero": "303",
+            "nom": "STRONGER WITH YOU",
+            "marque": "ARMANI"
+        },
+        {
+            "numero": "515",
+            "nom": "ROSE D'ARABIE - PRIVÉ",
+            "marque": "ARMANI"
+        },
+        {
+            "numero": "312",
+            "nom": "CHROME",
+            "marque": "AZZARO"
+        },
+        {
+            "numero": "504",
+            "nom": "CK ONE",
+            "marque": "CALVIN KLEIN"
+        },
+        {
+            "numero": "305",
+            "nom": "BLEU",
+            "marque": "CHANEL"
+        },
+        {
+            "numero": "306",
+            "nom": "ALLURE",
+            "marque": "CHANEL"
+        },
+        {
+            "numero": "324",
+            "nom": "EGOISTE",
+            "marque": "CHANEL"
+        },
+        {
+            "numero": "308",
+            "nom": "DIOR HOMME",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "309",
+            "nom": "FARENHEIT",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "310",
+            "nom": "SAUVAGE",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "333",
+            "nom": "EAU SAUVAGE",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "510",
+            "nom": "BOIS D'ARGENT",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "512",
+            "nom": "OUD ISPAHAN",
+            "marque": "CHRISTIAN DIOR"
+        },
+        {
+            "numero": "505",
+            "nom": "BACCARAT ROUGE 540",
+            "marque": "FRANCIS KURKDJIAN"
+        },
+        {
+            "numero": "314",
+            "nom": "TERRE D'HERMES",
+            "marque": "HERMES"
+        },
+        {
+            "numero": "322",
+            "nom": "BOSS",
+            "marque": "HUGO BOSS"
+        },
+        {
+            "numero": "315",
+            "nom": "LE MALE",
+            "marque": "JEAN PAUL GAULTIER"
+        },
+        {
+            "numero": "327",
+            "nom": "LE BEAU",
+            "marque": "JEAN PAUL GAULTIER"
+        },
+        {
+            "numero": "329",
+            "nom": "SCANDAL",
+            "marque": "JEAN PAUL GAULTIER"
+        },
+        {
+            "numero": "331",
+            "nom": "ULTRA MALE",
+            "marque": "JEAN PAUL GAULTIER"
+        },
+        {
+            "numero": "317",
+            "nom": "INVICTUS",
+            "marque": "PACO RABANNE"
+        },
+        {
+            "numero": "318",
+            "nom": "PURE XS",
+            "marque": "PACO RABANNE"
+        },
+        {
+            "numero": "328",
+            "nom": "ONE MILLION",
+            "marque": "PACO RABANNE"
+        },
+        {
+            "numero": "325",
+            "nom": "NOIR EXTREME",
+            "marque": "TOM FORD"
+        },
+        {
+            "numero": "501",
+            "nom": "AMBER ABSOLUTE",
+            "marque": "TOM FORD"
+        },
+        {
+            "numero": "503",
+            "nom": "TUSCAN LEATHER",
+            "marque": "TOM FORD"
+        },
+        {
+            "numero": "506",
+            "nom": "FUCKING FABULOUS",
+            "marque": "TOM FORD"
+        },
+        {
+            "numero": "508",
+            "nom": "OMBRÉ LEATHER",
+            "marque": "TOM FORD"
+        },
+        {
+            "numero": "509",
+            "nom": "OUD WOOD",
+            "marque": "TOM FORD"
+        },
+        {
+            "numero": "320",
+            "nom": "L'HOMME",
+            "marque": "YVES SAINT LAURENT"
+        },
+        {
+            "numero": "326",
+            "nom": "LA NUIT DE L'HOMME",
+            "marque": "YVES SAINT LAURENT"
+        },
+        {
+            "numero": "330",
+            "nom": "KOUROS",
+            "marque": "YVES SAINT LAURENT"
+        },
+        {
+            "numero": "339",
+            "nom": "MYSLF",
+            "marque": "YVES SAINT LAURENT"
+        }
+    ]
+}
---
+++ js/main.js
@@ -0,0 +1,44 @@
+function openModal(type) {
+    document.getElementById('modal' + type.charAt(0).toUpperCase() + type.slice(1)).style.display = 'block';
+    document.body.style.overflow = 'hidden';
+}
+
+function closeModal(type) {
+    document.getElementById('modal' + type.charAt(0).toUpperCase() + type.slice(1)).style.display = 'none';
+    document.body.style.overflow = 'auto';
+}
+
+window.onclick = function (event) {
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    }
+}
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+        document.querySelectorAll('.modal').forEach(modal => {
+            modal.style.display = 'none';
+        });
+        document.body.style.overflow = 'auto';
+    }
+});
+
+function createParfumList(type) {
+    const list = document.getElementById(type + 'List');
+    list.innerHTML = '';
+    parfumsData[type].forEach(parfum => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <span class="parfum-numero">${parfum.numero}</span>
+            <span class="parfum-name">${parfum.nom}</span>
+            <span class="parfum-brand">${parfum.marque}</span>
+        `;
+        list.appendChild(li);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    createParfumList('homme');
+    createParfumList('femme');
+});
